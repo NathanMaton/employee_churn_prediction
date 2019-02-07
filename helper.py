b0fce5f9fc8e119362b5964827b68e058f244ce7
Nathan Maton <nathanmaton@gmail.com>
2019-02-07 12:57:53 -0800

    swapped from cv to doing most scoring with recall and putting dupes in training set

--- helper.py
+++ helper.py
@@ -24,13 +24,39 @@ def clean_churn_df(df):
     return df
 
 def score_model(model, X_train, y_train):
-    cv_results = cross_validate(model, X_train, y_train, cv=5, return_train_score=False)
+    cv_results = cross_validate(model, X_train, y_train, cv=5, scoring='recall')
     return np.mean(cv_results['test_score'])
 
-def model_baseline(X_train, y_train):
-    #lm1 = LogisticRegression()     #1 feature logistic regression
-    #lm1_score = score_model(lm1, X_train['satisfaction_level'], y_train)
+def score_model_no_cv(model, X_train, y_train, X_val, y_val):
+    model.fit(X_train, y_train)
+    return recall_score(model.predict(X_val), y_val)
 
+
+def model_baseline_no_cv(X_train, y_train, X_val, y_val):
+    lm2 = LogisticRegression() #all features
+    lm2_score = score_model_no_cv(lm2, X_train, y_train, X_val, y_val)
+
+    knn = KNeighborsClassifier(n_neighbors=5)
+    knn_score = score_model_no_cv(knn, X_train, y_train, X_val, y_val)
+
+    gnb = GaussianNB()
+    gnb_score = score_model_no_cv(gnb, X_train, y_train, X_val, y_val)
+
+    mnb = MultinomialNB()
+    mnb_score = score_model_no_cv(mnb, X_train, y_train, X_val, y_val)
+
+    svm_model = svm.SVC()
+    svm_score = score_model_no_cv(svm_model, X_train, y_train, X_val, y_val)
+
+    rf = RandomForestClassifier()
+    rf_score = score_model_no_cv(rf, X_train, y_train, X_val, y_val)
+
+    #maybe try XGB for fun?
+    #clf = XGBClassifier().fit(X_train, y_train)
+
+    return lm2_score, knn_score, gnb_score, mnb_score, svm_score, rf_score
+
+def model_baseline(X_train, y_train):
     lm2 = LogisticRegression() #all features
     lm2_score = score_model(lm2, X_train, y_train)
 
@@ -53,6 +79,33 @@ def model_baseline(X_train, y_train):
     #clf = XGBClassifier().fit(X_train, y_train)
 
     return lm2_score, knn_score, gnb_score, mnb_score, svm_score, rf_score
+
+def split_with_dupe_rows_in_train(churn_df):
+    #find dupes
+    churn_df_dupes = churn_df[churn_df.duplicated()==True]
+    rest_df = churn_df[churn_df.duplicated()==False]
+
+    #randomly split the non dupes
+    X_train, X_holdout, y_train, y_holdout = train_test_split(rest_df, rest_df['left'], \
+                                                            test_size=0.2, random_state=41)
+    #set aside a validation set (approx. 20%)
+    X_val = X_train.iloc[:2000,:]
+    y_val = y_train.iloc[:2000]
+    X_train = X_train.iloc[2000:,:]
+    y_train = y_train.iloc[2000:]
+
+    #add dupes into training data
+    y_dupes = churn_df_dupes['left']
+    y_train = y_train.append(y_dupes)
+    X_train = pd.concat([X_train, churn_df_dupes])
+
+    #clean your data
+    X_train = clean_churn_df(X_train)
+    X_val = clean_churn_df(X_val)
+    X_holdout = clean_churn_df(X_holdout)
+
+    return X_train, X_val, X_holdout, y_train, y_val, y_holdout
+
 
 def RMSE(validation_points, prediction_points):
    """
